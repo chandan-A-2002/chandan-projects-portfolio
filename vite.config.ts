6ef3e78c1e688e0db2f132261d80a51d3fbc080c
chandan-A-2002 <chandanahirwar2002@gmail.com>
2025-06-10 20:12:40 +0000

    issue resolved

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,31 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { writeFileSync, readFileSync } from "fs";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const config = {
+    base: "/chandan-projects-portfolio/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' && componentTagger(),
+      {
+        name: 'copy-index-to-404',
+        closeBundle() {
+          const indexHtml = readFileSync('dist/index.html');
+          writeFileSync('dist/404.html', indexHtml);
+        },
+      }
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+  return config;
+});
